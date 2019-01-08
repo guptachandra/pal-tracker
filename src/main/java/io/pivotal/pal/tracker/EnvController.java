03f9c221e662376b7c93fcf2aec7b603223c784a
Yogesh Shetty <yogesh.shetty1@t-mobile.com>
2019-01-08 08:40:21 -0700

    add application class

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,3 +1,5 @@
+package io.pivotal.pal.tracker;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,7 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-class EnvController {
+public class EnvController {
 
     private final String port;
     private final String memoryLimit;
